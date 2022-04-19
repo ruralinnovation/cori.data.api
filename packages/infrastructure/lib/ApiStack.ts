31d04731aff04b1d569f1575ba6f5f099592be17
Nahum <nahumsmith@gmail.com>
2022-04-19 08:06:07 -0400

    integrate working infra code from separate dir

--- packages/infrastructure/lib/ApiStack.ts
+++ packages/infrastructure/lib/ApiStack.ts
@@ -169,7 +169,7 @@ export class ApiStack extends Stack {
       ...defaults,
       role: this.iam.roles.testApiRole,
       timeout: Duration.seconds(360),
-      entry: resolve("../", "python-lambdas/testApi"),
+      entry: resolve("../../../", "python-microservices/testApi"),
     }).addPaths({
       "/api/hello": ["GET"],
       "/api/hello/{name}": ["GET"],
