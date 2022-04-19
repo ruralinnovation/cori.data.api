cf04b446f84318d720517b56892518d6b56ecef8
Nahum <nahum@mergingfutures.com>
2022-04-19 09:45:25 -0400

    ongoing schema setup

--- packages/schemas/index.ts
+++ packages/schemas/index.ts
@@ -1,5 +1,5 @@
 import { join } from "path";
-import { writeFileSync } from "fs";
+import { writeFileSync, existsSync, mkdirSync } from "fs";
 import { loadSchemaSync } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { addResolversToSchema } from "@graphql-tools/schema";
@@ -12,4 +12,8 @@ const schema = loadSchemaSync("./graphql/**/*.graphql", {
 
 console.log("Checking ... ", printSchema(schema));
 
+if (!existsSync("dist")) {
+  mkdirSync("dist");
+}
 writeFileSync("schema.graphql", printSchema(schema));
+writeFileSync("dist/schema.graphql", printSchema(schema));
