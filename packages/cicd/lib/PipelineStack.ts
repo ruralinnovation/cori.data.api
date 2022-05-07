411777ad07ca391515016072ee70615915b63709
Nahum <nahum@mergingfutures.com>
2022-05-07 16:30:31 -0400

    cicd scripting

--- packages/cicd/lib/PipelineStack.ts
+++ packages/cicd/lib/PipelineStack.ts
@@ -22,7 +22,7 @@ export class CICDPipelineStack extends Stack {
       pipelineName: `${props.client}-${props.project}-cicdpipeline-${props.stage}`,
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub(props.repo, props.branch),
-        commands: ['cd packages/cicd', 'npm ci', 'npm run build', 'npx cdk synth'],
+        commands: ['npm ci', 'npm run build:cicd', 'npm run synth:cicd'],
         primaryOutputDirectory: 'packages/cicd/cdk.out',
       }),
     });
