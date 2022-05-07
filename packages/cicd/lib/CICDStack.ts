11af3c190fbb726fabd35bc160afe598adfda10e
Nahum <nahum@mergingfutures.com>
2022-05-07 19:42:45 -0400

    update

--- packages/cicd/lib/CICDStack.ts
+++ packages/cicd/lib/CICDStack.ts
@@ -33,15 +33,18 @@ export class CICDStack extends Stack {
             'npm run synth:cicd',
           ],
           primaryOutputDirectory: 'packages/cicd/cdk.out',
+          env: {
+            privileged: 'True',
+          },
         }),
       });
 
-      pipeline.addStage(
-        new AppStage(this, 'DeployApiResources', {
-          env: { account: accountNumber, region: region },
-          stage: env.stage,
-        })
-      );
+      // pipeline.addStage(
+      //   new AppStage(this, `${env.stage}_DeployApiResources`, {
+      //     env: { account: accountNumber, region: region },
+      //     stage: env.stage,
+      //   })
+      // );
     });
   }
 }
