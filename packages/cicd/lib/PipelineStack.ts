72c4c72193e2bd009e1ea579c5ec51a964ec5789
Nahum <nahum@mergingfutures.com>
2022-05-07 16:13:07 -0400

    testing

--- packages/cicd/lib/PipelineStack.ts
+++ packages/cicd/lib/PipelineStack.ts
@@ -22,7 +22,7 @@ export class CICDPipelineStack extends Stack {
       pipelineName: `${props.client}-${props.project}-cicdpipeline-${props.stage}`,
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub(props.repo, props.branch),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth'],
+        commands: ['npm i'],
       }),
     });
   }
