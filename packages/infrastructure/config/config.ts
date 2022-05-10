bbaa6bb8596f861d5ac7161db342608abbd03d5a
Nahum <nahum@mergingfutures.com>
2022-05-10 09:38:44 -0400

    testing

--- packages/infrastructure/config/config.ts
+++ packages/infrastructure/config/config.ts
@@ -56,6 +56,25 @@ export const Config: IConfigs = {
     },
     databaseConfig: {
       vpcId: 'vpc-0499b35a2f5231aae',
+      databaseSecurityGroupId: 'sg-0cdb6d6c66b8e6a4a',
+      host: 'cori-testing-db.c0no2rvbbm4n.us-east-1.rds.amazonaws.com',
+      dbname: 'postgres',
+      parameterName: 'cori-data-api-read_only_user_password',
+      dbuser: 'read_only_user',
+    },
+  },
+  'dev1': {
+    client: 'cori',
+    stage: 'dev',
+    project: 'data-api',
+    loggingLevel: 'info',
+    retain: false,
+    env: {
+      account: '190686435752',
+      region: 'us-east-1',
+    },
+    databaseConfig: {
+      vpcId: 'vpc-0499b35a2f5231aae',
       databaseSecurityGroupId: 'sg-0be66ca1818bcc0e0',
       host: 'cori-database-small-dev.c0no2rvbbm4n.us-east-1.rds.amazonaws.com',
       dbname: 'data1',
