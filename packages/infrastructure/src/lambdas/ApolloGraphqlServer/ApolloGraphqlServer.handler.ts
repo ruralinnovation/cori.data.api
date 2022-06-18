2e86e0aebe4e2e5fa52676b4153801095d403c8b
Nahum <nahum@mergingfutures.com>
2022-06-18 16:26:28 -0400

    update apollo bcat custom queries

--- packages/infrastructure/src/lambdas/ApolloGraphqlServer/ApolloGraphqlServer.handler.ts
+++ packages/infrastructure/src/lambdas/ApolloGraphqlServer/ApolloGraphqlServer.handler.ts
@@ -4,9 +4,10 @@ import { PythonRestApi } from './datasources';
 import { mergeSchemas } from '@graphql-tools/schema';
 import GeoJSON from './schema/geojson';
 import { Cache, checkCache } from './cache';
-import { GraphQLList, GraphQLString } from 'graphql';
+import { GraphQLBoolean, GraphQLList, GraphQLString } from 'graphql';
 const compression = require('compression');
 const express = require('express');
+import { bcatQueries } from './schema/bcatQueries';
 
 const cache = new Cache();
 
@@ -24,44 +25,67 @@ const RootQuery = new GraphQLObjectType({
       type: GeoJSON.FeatureCollectionObject,
       args: {
         table: {
-          type: GraphQLString,
+          type: GraphQLString!,
         },
         counties: {
           type: new GraphQLList(GraphQLString),
         },
+        state_abbr: {
+          type: GraphQLString,
+        },
+        skipCache: {
+          type: GraphQLBoolean,
+        },
       },
-      resolve: async (_: any, { table, counties }: any, { dataSources, redisClient }: any, info: any) => {
-        return await counties.reduce(
-          async (fc, county) => {
-            const featureCollection = await fc;
-            // const res: any = await redisClient.checkCache(`${table}-${county}`, async () => {
-            //   return await dataSources.pythonApi.getItem(`bcat/${table}/geojson?geoid_co=${county}`);
-            // });
-            const res: any = await dataSources.pythonApi.getItem(`bcat/${table}/geojson?geoid_co=${county}`);
-            if (res) {
-              return {
-                ...featureCollection,
-                features: featureCollection.features.concat(res.features),
-              };
-            } else {
-              return featureCollection;
-            }
-          },
-          Promise.resolve({
-            type: 'FeatureCollection',
-            features: [],
-          })
-        );
+      resolve: async (
+        _: any,
+        { table, state_abbr, counties, skipCache }: any,
+        { dataSources, redisClient }: any,
+        info: any
+      ) => {
+        if (state_abbr) {
+          return skipCache
+            ? await dataSources.pythonApi.getItem(`bcat/${table}/geojson?state_abbr=${state_abbr}`)
+            : await redisClient.checkCache(`${table}-${state_abbr}`, async () => {
+                return await dataSources.pythonApi.getItem(`bcat/${table}/geojson?state_abbr=${state_abbr}`);
+              });
+        } else {
+          if (!counties) {
+            throw new Error('When no state abbr is specified you MUSt filter by state_abbr');
+          }
+          return await counties.reduce(
+            async (fc, county) => {
+              const featureCollection = await fc;
+              const res: any = skipCache
+                ? await redisClient.checkCache(`${table}-${county}`, async () => {
+                    return await dataSources.pythonApi.getItem(`bcat/${table}/geojson?geoid_co=${county}`);
+                  })
+                : await dataSources.pythonApi.getItem(`bcat/${table}/geojson?geoid_co=${county}`);
+              if (res) {
+                return {
+                  ...featureCollection,
+                  features: featureCollection.features.concat(res.features),
+                };
+              } else {
+                return featureCollection;
+              }
+            },
+            Promise.resolve({
+              type: 'FeatureCollection',
+              features: [],
+            })
+          );
+        }
       },
     },
     county_feature: {
       type: GeoJSON.FeatureCollectionObject,
       args: {
         table: {
-          type: GraphQLString,
+          type: GraphQLString!,
         },
         county: {
-          type: GraphQLString,
+          type: GraphQLString!,
         },
       },
       resolve: async (_: any, { table, county }: any, { dataSources, redisClient }: any, info: any) => {
@@ -71,6 +95,7 @@ const RootQuery = new GraphQLObjectType({
         });
       },
     },
+    ...bcatQueries,
   },
 });
 const schema = mergeSchemas({
