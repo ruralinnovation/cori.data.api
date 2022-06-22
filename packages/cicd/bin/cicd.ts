223c6cf9bd864981f52e3c186c344916e2384020
Ivan Bliskavka <ibliskavka@live.com>
2022-06-22 17:56:37 -0400

    webhook trigger

--- packages/cicd/bin/cicd.ts
+++ packages/cicd/bin/cicd.ts
@@ -13,10 +13,11 @@ const defaultEnv = { account: '190686435752', region: 'us-east-1' };
 const sourceConfig = {
   repo: 'mergingfutures/cori-data-api',
   authentication: cdk.SecretValue.secretsManager('github-token'),
-  trigger: GitHubTrigger.POLL,
+  trigger: GitHubTrigger.WEBHOOK,
 };
 
 // Each pipeline creates a new bucket, which can push us past the bucket quota. This allows us to re-use an existing one.
+// @todo: create a bucket with a prettier name
 const artifactBucketName = 'coridataapicicdstack-devpipelineartifactsbucketfd-1smu59goaufdm';
 
 /**
@@ -36,6 +37,12 @@ function buildPipeline(stage: string, branch?: string): PipelineStack {
     ApiConfig: getConfig(stage),
   });
 }
+
+/**
+ * Create pipelines per branch/stage here.
+ * You can use cross account deploys to push build in the MF account and deploy to CORI
+ *  - Alternatively, you can deploy the CORI pipelines to their account by changing the defaultEnv
+ */
 
 buildPipeline('ib-dev', 'refactor/ibliskavka-pipelines')
 
