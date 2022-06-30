dd894d0397e9a184d9487f923787900e20446808
Ivan Bliskavka <ibliskavka@live.com>
2022-06-30 18:59:38 -0400

    Access version info

--- packages/infrastructure/src/util.ts
+++ packages/infrastructure/src/util.ts
@@ -1,3 +1,8 @@
+import * as pkgJson from '../package.json';
+
+export const VERSION = pkgJson.version;
+export const PROJECT_ROOT = __dirname;
+
 /**
  * Type-safe way to access object keys
  * https://stackoverflow.com/a/67452316
