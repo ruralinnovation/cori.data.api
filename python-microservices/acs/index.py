a298073db10829be107bdec77edf6ea97d82320a
John Hall <john@real-currents.com>
2022-07-28 14:12:20 -0400

    add first new python microservice

---
+++ python-microservices/acs/index.py
@@ -0,0 +1,34 @@
+import os
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.logging import correlation_paths
+from aws_lambda_powertools.event_handler.api_gateway import APIGatewayRestResolver, Response
+from aws_lambda_powertools.event_handler.exceptions import BadRequestError
+
+
+logger = Logger(service="ACSService")
+tracer = Tracer(service="ACSService")
+app = APIGatewayRestResolver(strip_prefixes=["/acs"])
+
+
+@app.get(rule="/bad-request-error")
+def bad_request_error(msg):
+    # HTTP  400
+    raise BadRequestError(msg)
+
+
+@app.get("/testing", compress=False)
+def get():
+    print("testing endpoint /testing")
+    return {
+        "message": "success"
+    }
+
+# @app.get("/<table>/geojson", compress=False)
+# def get(table):
+#     print(table)
+
+# You can continue to use other utilities just as before
+@tracer.capture_lambda_handler
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST, log_event=True)
+def handler(event, context):
+    return app.resolve(event, context)
