b36a765086022d8009958e3c8dcde34aeeb9d039
John Hall <john.hall@ruralinnovation.us>
2023-12-16 08:39:37 -0500

    add type defs to commonjs dist

--- inst/dist/@cori-risi/contexts/ApiContextProvider.d.ts
+++
@@ -1,5 +0,0 @@
-export default function ApiContextProvider(props: any): JSX.Element;
-export const ApiContext: import("react").Context<{
-    authenticated_user: null;
-}>;
-//# sourceMappingURL=ApiContextProvider.d.ts.map
