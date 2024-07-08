1d668bdf37b5e37e4d6235dd4d1880bd811047c1
John Hall <john.hall@ruralinnovation.us>
2024-07-07 20:14:40 -0400

    export api context

--- inst/dist/cori.data.api.js
+++ inst/dist/cori.data.api.js
@@ -5,7 +5,7 @@
  * @license ISC
  */
 export { Button, Input, Label } from './@cori-risi/components/basic.js';
-export { default as ApiContextProvider } from './@cori-risi/cotexts/ApiContextProvider.js';
+export { ApiContext, default as ApiContextProvider } from './@cori-risi/cotexts/ApiContextProvider.js';
 export { default as CategoricalLegend } from './@cori-risi/components/CategoricalLegend.js';
 export { default as LineChart } from './@cori-risi/components/LineChart.js';
 
--- lib/cori.data.api.ts
+++ lib/cori.data.api.ts
@@ -12,7 +12,7 @@ export {
 } from './@cori-risi/components/basic';
 
 // Actual API
-export { default as ApiContextProvider } from './@cori-risi/cotexts/ApiContextProvider';
+export { default as ApiContextProvider, ApiContext } from './@cori-risi/cotexts/ApiContextProvider';
 // export { default as BarChart } from './@cori-risi/components/BarChart';
 export { default as CategoricalLegend } from './@cori-risi/components/CategoricalLegend';
 export { default as LineChart } from './@cori-risi/components/LineChart';
