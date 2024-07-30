25637f226416c5844903265ec4cc496d548c4d76
John Hall <john.hall@ruralinnovation.us>
2024-07-30 11:59:11 -0400

    document amplify context provider

--- inst/dist/@cori-risi/components/basic.d.ts
+++ inst/dist/@cori-risi/components/basic.d.ts
@@ -1,11 +1,35 @@
-import React, { ReactElement } from "react";
+import React, { ChangeEventHandler, ReactElement } from "react";
+/**
+ * This comment _supports_ [Markdown](https://www.markdownguide.org/)
+ *
+ * ```ts
+ * <Button onClick={changeLayer} ><>
+ *         Show "Served" tracts
+ * </></Button>
+ * ```
+ */
 export declare function Button(props: {
     children?: ReactElement;
     onClick: () => void;
 }): React.JSX.Element;
+/**
+ * This comment _supports_ [Markdown](https://www.markdownguide.org/)
+ *
+ * ```ts
+ * <Input checked={served}
+ *        onChange={() => checkServed(!served)}
+ *        type="checkbox" />
+ * ```
+ */
 export declare function Input(props: {
     children?: ReactElement;
+    type: string;
+    checked: boolean;
+    onChange?: ChangeEventHandler<HTMLInputElement> | undefined;
 }): React.JSX.Element;
+/**
+ * This comment _supports_ [Markdown](https://www.markdownguide.org/)
+ */
 export declare function Label(props: {
     children?: ReactElement;
 }): React.JSX.Element;
--- lib/@cori-risi/cotexts/AmplifyContextProvider.tsx
+++ lib/@cori-risi/cotexts/AmplifyContextProvider.tsx
@@ -38,7 +38,31 @@ const initAmplifyContext: AmplifyContextType = {
 
 export const AmplifyContext = createContext<AmplifyContextType | null>(initAmplifyContext);
 
-export default function AmplifyContextProvider(props: {
+/**
+ * This component provides the Amplify configuration context to an Amplify app,
+ * which is particularly useful to one that requires authentication in order to
+ * access the CORI Data API. Other than the children prop, the parameters are
+ * [AWS Cognito](https://us-east-1.console.aws.amazon.com/cognito/v2/home?region=us-east-1){target=_blank}
+ * values, passed so that the Amplify configuration method can use the specified User pool
+ * and Identity pool to authenticate users (including the `cori-risi-public` user).
+ *
+ * ```ts
+ * <AmplifyContextProvider domain={import.meta.env.VITE_COGNITO_DOMAIN}
+ *                         region={import.meta.env.VITE_REGION}
+ *                         identityPoolId={import.meta.env.VITE_IDENTITY_POOL_ID}
+ *                         userPoolId={import.meta.env.VITE_USER_POOL_ID}
+ *                         userPoolClientId={import.meta.env.VITE_USER_POOL_CLIENT_ID} >
+ *     <App />
+ * </AmplifyContextProvider>
+ * ```
+ *
+ *  @param props.domain - domain for the Cognito Hosted UI and/or OAuth 2.0 endpoints
+ *  @param props.region - AWS region where Cognito service is hosted
+ *  @param props.identityPoolId - Cognito Identity pool ID
+ *  @param props.userPoolId - Cognito User pool ID
+ *  @param props.userPoolClientId - ACognito User pool App client ID (App clients are the user pool authentication resources attached to your app).
+ */
+export default function AmplifyContextProvider (props: {
     children?: ReactElement,
     domain?: string,
     region?: string,
