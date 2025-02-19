6c43c9cc622bab7f3676ddd2178f76074ad43f0c
John Hall <john.hall@ruralinnovation.us>
2025-02-19 14:31:57 -0500

    target es2017 to get rid of tslib reference

--- inst/dist/@cori-risi/mapbox/index.js
+++ inst/dist/@cori-risi/mapbox/index.js
@@ -4,7 +4,6 @@
  * @copyright Rural Innovation Strategies, Inc.
  * @license ISC
  */
-import { __awaiter } from '../../_virtual/_tslib.js';
 import axios from 'axios';
 
 /**
@@ -22,39 +21,37 @@ import axios from 'axios';
  *
  * ```
  * */
-function mapboxGeocode(api_key, text) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const search_request = 'https://api.mapbox.com/search/geocode/v6/forward';
-        console.log("MAPBOX API TOKEN: ", api_key);
-        const res = yield axios.get(search_request, {
-            params: {
-                access_token: api_key,
-                q: text
-            }
-        });
-        console.log("MAPBOX API: ", res);
-        const found_features = res.data.features
-            .filter((f) => (f.hasOwnProperty("geometry") && f.geometry.type === "Point"));
-        console.log("found_features: ", found_features);
-        return found_features
-            .filter((item) => {
-            return item.hasOwnProperty("properties")
-                && item["properties"].hasOwnProperty("full_address")
-                && item["properties"].hasOwnProperty("context")
-                && item["properties"]["context"].hasOwnProperty("country")
-                && item["properties"]["context"]["country"].hasOwnProperty("country_code")
-                && item["properties"]["context"]["country"]["country_code"]
-                    .match(/(?:US|AS|GU|MP|PR|VI)/) !== null
-                && "region" in item["properties"]["context"]
-                && "postcode" in item["properties"]["context"];
-        })
-            .map((item) => {
-            console.log(item);
-            const properties = item["properties"];
-            properties["label"] = item["properties"]["full_address"];
-            properties['props'] = JSON.stringify(properties);
-            return Object.assign(Object.assign({}, item), { properties });
-        });
+async function mapboxGeocode(api_key, text) {
+    const search_request = 'https://api.mapbox.com/search/geocode/v6/forward';
+    console.log("MAPBOX API TOKEN: ", api_key);
+    const res = await axios.get(search_request, {
+        params: {
+            access_token: api_key,
+            q: text
+        }
+    });
+    console.log("MAPBOX API: ", res);
+    const found_features = res.data.features
+        .filter((f) => (f.hasOwnProperty("geometry") && f.geometry.type === "Point"));
+    console.log("found_features: ", found_features);
+    return found_features
+        .filter((item) => {
+        return item.hasOwnProperty("properties")
+            && item["properties"].hasOwnProperty("full_address")
+            && item["properties"].hasOwnProperty("context")
+            && item["properties"]["context"].hasOwnProperty("country")
+            && item["properties"]["context"]["country"].hasOwnProperty("country_code")
+            && item["properties"]["context"]["country"]["country_code"]
+                .match(/(?:US|AS|GU|MP|PR|VI)/) !== null
+            && "region" in item["properties"]["context"]
+            && "postcode" in item["properties"]["context"];
+    })
+        .map((item) => {
+        console.log(item);
+        const properties = item["properties"];
+        properties["label"] = item["properties"]["full_address"];
+        properties['props'] = JSON.stringify(properties);
+        return Object.assign(Object.assign({}, item), { properties });
     });
 }
 
